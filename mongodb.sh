11c4360a2ce80e3e3908015969764fe9081bbdda
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-04 13:49:31 +0530

    changes

--- mongodb.sh
+++ mongodb.sh
@@ -1,21 +1,22 @@
 source ./common.sh
 
 check_root
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+validate $? "copying" 
 
-dnf install mongodb-org -y 
-validate $? "installing mongodb" &>>$log_file
+dnf install mongodb-org -y &>>$log_file
+validate $? "installing mongodb" 
 
-systemctl enable mongod
-validate $? "enabling mongodb" &>>$log_file
+systemctl enable mongod &>>$log_file
+validate $? "enabling mongodb" 
 
-systemctl start mongod 
-validate $? "starting mongodb" &>>$log_file
+systemctl start mongod &>>$log_file
+validate $? "starting mongodb" 
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-validate $? "allowing remote connections" &>>$log_file
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$log_file
+validate $? "allowing remote connections" 
 
-systemctl restart mongod
-validate $? "restarting mongodb" &>>$log_file
+systemctl restart mongod &>>$log_file
+validate $? "restarting mongodb" 
 
 total_time
